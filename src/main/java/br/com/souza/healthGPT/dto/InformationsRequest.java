5affb17e4a91ef8069beeed111baca61ce1ff905
Vitor Souza <vitor.souza@absoftware.com.br>
2023-05-18 12:40:27 -0300

    Adicionando @NotBlank no request

--- src/main/java/br/com/souza/healthGPT/dto/InformationsRequest.java
+++ src/main/java/br/com/souza/healthGPT/dto/InformationsRequest.java
@@ -1,14 +1,19 @@
 package br.com.souza.healthGPT.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class InformationsRequest {
 
+    @NotBlank
     private int idade;
+    @NotBlank
     private BigDecimal peso;
+    @NotBlank
     private BigDecimal altura;
+    @NotBlank
     private String objetivo;
 
 }
